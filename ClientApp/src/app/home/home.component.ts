a9f2c897eee1254e2696c1306d61576f216d85c5
Diego Felisberto <diegojrf@msn.com>
2020-09-29 23:17:56 -0300

    .

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+  selector: "app-home",
+  templateUrl: "./home.component.html",
 })
 export class HomeComponent {
+  faCoffee = faCoffee;
 }
